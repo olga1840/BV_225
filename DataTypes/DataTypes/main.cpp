6b4cf29dd1bd57ec06537b6e5fae809b72798376
Clayman <Clayman@DESKTOP-DM8E77U>
2022-07-07 21:26:30 +0300

    DataTypes day 2

--- DataTypes/DataTypes/main.cpp
+++ DataTypes/DataTypes/main.cpp
@@ -5,8 +5,11 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "Russian");
-	cout << true << endl;	//endl - end line
-	cout << false << endl;
+	//cout << true << endl;	//endl - end line
+	//cout << false << endl;
 	//0 - ýòî false, à true - ýòî âñå ÷òî ÍÅ 0
-	cout << (bool)-0.0000000000001 << endl;
+	cout << sizeof(int) << endl;
+	//ÌÀÊÐÎÎÏÐÅÄÅËÅÍÈÉ â Visual Studio
+	cout << INT_MIN << " ... " << INT_MAX << endl;
+	cout << 0 << " ... " << UINT_MAX << endl;
 }
