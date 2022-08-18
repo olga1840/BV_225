17bba7ea4606cc2031101da4628fb81bead8576c
Clayman <Clayman@DESKTOP-DM8E77U>
2022-08-18 21:23:46 +0300

    while

--- ControlStructures/ControlStructures/main.cpp
+++ ControlStructures/ControlStructures/main.cpp
@@ -4,9 +4,17 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+//#define TEMPERATURE
+//#define TARGET
+//#define CALC_1
+//define - определить
+//definition - определение
+//#define SWITCH_SYNTAX
+
 void main()
 {
 	setlocale(LC_ALL, "Rus");
+
 #if defined TEMPERATURE
 	int temperature;
 	cout << "Введите температуру воздуха: "; cin >> temperature;
@@ -20,6 +28,7 @@ void main()
 	}
 #endif // TEMPERATURE
 
+#ifdef TARGET
 	int n;
 	cout << "Введите число: "; cin >> n;
 	if (n > 0 && n < 10)
@@ -30,4 +39,64 @@ void main()
 	{
 		cout << "Вы промахнулись" << endl;
 	}
+#endif // TARGET
+
+#if defined CALC_1	//Начало блока
+//если определено CALC_1, то нижеследующий код, до директивы #endif,
+//будет виден компилятору
+	double a, b;	//Числа, вводимые с клавиатуры
+	char s;			//Sign - знак операции
+	cout << "Введите простое арифметическое выражение: ";
+	cin >> a >> s >> b;
+	//cout << a << s << b << endl;
+	if (s == '+')
+	{
+		cout << a << " + " << b << " = " << a + b << endl;
+	}
+	else if (s == '-')
+	{
+		cout << a << " - " << b << " = " << a - b << endl;
+	}
+	else if (s == '*')
+	{
+		cout << a << " * " << b << " = " << a * b << endl;
+	}
+	else if (s == '/')
+	{
+		cout << a << " / " << b << " = " << a / b << endl;
+	}
+	else
+	{
+		cout << "Error: No operation!" << endl;
+	}
+#endif				//Конец блока
+
+#ifdef SWITCH_SYNTAX
+	int var = 0;
+	int code1 = 0;
+	int code2 = 0;
+	int codeN = 0;
+	int default_code = 0;
+#define CONST_1	1
+#define CONST_2	2
+#define CONST_N	'N'
+
+
+	//Конструкция множественного выбора switch
+	//if, который позволяет выбрать один из двух вариантов кода, в зависимости от условия,
+	//switch позволяет выьрать один из множества вариантов кода, в зависимости от значения некоторой переменной.
+
+
+	switch (var)
+	{
+	case CONST_1: code1; break;
+	case CONST_2: code2; break;
+		//........................
+		//........................
+	case CONST_N: codeN; break;
+	default: default_code;
+	}
+#endif // SWITCH_SYNTAX
+
+
 }
---
+++ ControlStructures/HomeWork/Source.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+
+void main() {
+	setlocale(LC_ALL, "");
+	int num = 0;
+	cout << ""; std::cin >> num;
+	string flag = "□";
+	for (short i = 0; i < 5; i++) {
+		for (short j = 0; j < 5; j++) {
+			cout << flag;
+			if (flag == "□")
+				flag = "■";
+			else flag = "□";
+		}
+		cout << endl;
+		if (i % 2 == 0) {
+			flag = "■";
+		}
+		else flag = "□";
+	}
+}
