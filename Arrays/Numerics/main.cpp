c28906324485a72af291f97a7c64695e2c5b4c0f
Clayman <Clayman@DESKTOP-DM8E77U>
2022-10-26 21:03:57 +0300

    Numerics DONE

--- Arrays/Numerics/main.cpp
+++ Arrays/Numerics/main.cpp
@@ -1,4 +1,4 @@
-﻿//Numerics
+﻿//Numericsi
 #include<iostream>
 using namespace std;
 
@@ -8,12 +8,14 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-#ifdef DEC_2_BIN
-	const int n = 32;
-	bool bin[n] = {};	//этот массив будет хранить разряды двоичного числа
+	//for (int i = 0; i < 256; i++)cout << i << "\t" << (char)i << endl;
 	int decimal;		//десятичное число, введенное с клавиатуры
 	cout << "Введите десятичное число: "; cin >> decimal;
 	cout << decimal << endl;
+
+#ifdef DEC_2_BIN
+	const int n = 32;
+	bool bin[n] = {};	//этот массив будет хранить разряды двоичного числа
 	int i = 0;	//номер разряда
 //	for (Start;	Stop; Step)
 	for (/*Пустое выражение*/; decimal; i++, decimal /= 2)
@@ -32,4 +34,34 @@ void main()
 	}
 #endif // DEC_2_BIN
 
+	const int n = 8;
+	char hex[n] = {};	//Этот массив будет хранить шестнадцатеричное число
+	int i = 0;
+	for (; decimal; i++, decimal /= 16)
+	{
+		hex[i] = decimal % 16;
+	}
+
+	for (--i; i >= 0; i--)
+	{
+		/*switch (hex[i])
+		{
+		case 10:cout << "A"; break;
+		case 11:cout << "B"; break;
+		case 12:cout << "C"; break;
+		case 13:cout << "D"; break;
+		case 14:cout << "E"; break;
+		cas
+e 15:cout << "F"; break;
+		default:cout << hex[i];
+		}*/
+
+		//if (hex[i] < 10)cout << hex[i];	else cout << char(hex[i] + 55);
+		//(hex[i] < 10) ? cout << hex[i] : cout << char(hex[i] + 55);
+		//cout << char(hex[i] < 10 ? hex[i] + 48 : hex[i] + 55);
+		//cout << char(hex[i] + (hex[i] < 10 ? 48 : 55));
+		hex[i] += hex[i] < 10 ? 48 : 55;
+		cout << hex[i];
+	}
+	cout << endl;
 }
