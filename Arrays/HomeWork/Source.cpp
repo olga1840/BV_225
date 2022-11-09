62885e61b9d47d713b57e4e40a977221d913e777
Clayman <Clayman@DESKTOP-DM8E77U>
2022-11-09 20:38:47 +0200

    Functions day 2

--- Arrays/HomeWork/Source.cpp
+++ Arrays/HomeWork/Source.cpp
@@ -1,33 +1,401 @@
-#include<iostream>
+// Arrays.cpp 
+
+#include <iostream>
+#include <time.h>
+#include <Windows.h>
+#include <conio.h>
+#include <stdlib.h>
 using namespace std;
+enum Color { Black = 0, Blue, Green, Cyan, Red, Magenta, Brown, LightGray, DarkGray, LightBlue, LightGreen, LightCyan, LightRed, LightMagenta, Yellow, White };
+enum Direction { Up = 72, Left = 75, Right = 77, Down = 80, Enter = 13, esc = 27, spase = 32 };
+HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);//получает дискриптор активного окна
 
-void main()
+void SetColor(Color text, Color background)
 {
-	setlocale(LC_ALL, "rus");
-
+	SetConsoleTextAttribute(hStdOut, (WORD)((background << 4) | text));
+}
+void SetCursor(int x, int y)
+{
+	COORD myCoords = { x,y };//инициализируем передаваемыми значениями объект координат
+	SetConsoleCursorPosition(hStdOut, myCoords);
+}
+int menu();
+void FillRand(int mas[], int n, int a, int b);
+void Prit(int mas[], int n);
+int Sum(int mas[], int n);
+int Avg(int Sum, int n);
+int minValueln(int mas[], int n);
+int maxValueln(int mas[], int n);
+void Sort(int mas[], int n);
+void UniqueRand(int mas[], int n, int a, int b);
+void Search(int mas[], int n);
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "RUS");
 	const int n = 10;
-	int arr[n] = { 1,2,3,4,5,6,7,8,9,10 };
-	for (int i = 0; i < n; i++)
+	int mas[n];
+	int a = 0, b = 100;
+
+
+
+	int poz = 0;
+	int key;
+	do
 	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-	int numb_ent;
-	cout << "Введите количество сдвигов: "; cin >> numb_ent;
-	for (int i = 0; i < numb_ent; i++)
-	{
-		int buffer = arr[n - 1];
-		for (int i = n - 1; i > 0; i--)
+		system("cls");
+		poz = menu();
+		system("cls");
+		switch (poz)
 		{
-			arr[i] = arr[i - 1];
+		case 1:
+		{
+
+
+
+			const int n = 10;
+			int mas[n];
+			int size = 0;
+			for (int i = 0; i < 10; i++)
+			{
+
+				mas[i] = i + 1;
+				cout << mas[i] << "\t";
+			}
+
+			do {
+				system("cls");
+
+				for (int i = 0; i < 10; i++)
+				{
+
+
+					cout << mas[i] << "\t";
+				}
+
+				cout << endl;
+				cout << "Нажмите Enter для выхода";
+
+
+
+				key = _getch();
+				switch (key)
+				{
+				case Left:
+				{
+					size--;
+					break;
+				}
+				case Right:
+				{
+					size++;
+					break;
+				}
+				case Enter:
+				{
+					break;
+				}
+
+				}
+				if (size < 0)
+				{
+					size = 9;
+				}
+				if (size > 9 && key == Right)
+				{
+					size = 0;
+				}
+				size = n - size;
+				for (int i = 0; i < size; i++)
+				{
+					int d = mas[0];
+					for (int i = 0; i < n; i++)
+					{
+
+						mas[i] = mas[i + 1];
+
+					}
+					mas[n - 1] = d;
+				}
+
+
+			} while (key != Enter);
+			break;
 		}
-		arr[0] = buffer;
-	}
+
+		case 0:
+		{
+			do {
+				system("cls");
+				cout << "Введите диапазон от "; cin >> a; cout << " до "; cin >> b;
+				system("cls");
+				FillRand(mas, n, a, b);
+				Prit(mas, n);
+				cout << endl << Sum(mas, n);
+				cout << endl << Avg(Sum(mas, n), n);
+				cout << endl << minValueln(mas, n);
+				cout << endl << maxValueln(mas, n);
+				cout << "\nESC-для Выхода";
+				key = _getch();
+			} while (key != esc);
+			break;
+
+		}
+
+		case 2:
+		{
+			do {
+				system("cls");
+				Sort(mas, n);
+				Prit(mas, n);
+				cout << endl;
+				cout << "\nESC-для Выхода";
+				key = _getch();
+			} while (key != esc);
+			break;
+		}
+
+		case 3:
+		{
+			do {
+				system("cls");
+				cout << "Введите диапазон от "; cin >> a; cout << "до "; cin >> b;
+				system("cls");
+
+				UniqueRand(mas, n, a, b);
+				Prit(mas, n);
+				cout << endl;
+				cout << "\nESC-для Выхода";
+				key = _getch();
+			} while (key != esc);
+			break;
+		}
+		case 4:
+		{
+			do
+			{
+				system("cls");
+				FillRand(mas, n, a, b);
+				Prit(mas, n);
+				Search(mas, n);
+				cout << endl;
+				cout << "\nESC-для Выхода";
+				key = _getch();
+			} while (key != esc);
+
+		}
+		case 5:
+		{
+			break;
+		}
+
+		}
+
+	} while (poz != 5);
+}
+
+void FillRand(int mas[], int n, int a, int b)
+{
+	srand(time(NULL));
 	for (int i = 0; i < n; i++)
 	{
-		cout << arr[i] << "\t";
+		mas[i] = a + rand() % (b - a);
+
+	}
+
+}
+
+void Prit(int mas[], int n)
+{
+	for (int i = -0; i < n; i++)
+	{
+		cout << mas[i] << "\t";
 	}
 	cout << endl;
+}
+
+int Sum(int mas[], int n)
+{
+	int Sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		Sum = Sum + mas[i];
+
+	}
+
+
+
+	return Sum;
+}
+
+int Avg(int Sum, int n)
+{
+	int Avg = 0;
+	Avg = Sum / n;
+
+	return Avg;
+}
+
+int minValueln(int mas[], int n)
+{
+	int Min = mas[0];
+
+	for (int i = 1; i < n; i++)
+	{
+		if (mas[i] < Min)
+		{
+			Min = mas[i];
+		}
+
+
+	}
+
+
+	return Min;
+}
+
+int maxValueln(int mas[], int n)
+{
+
+	int Max = mas[0];
+
+	for (int i = 1; i < n; i++)
+	{
+		if (mas[i] > Max)
+		{
+			Max = mas[i];
+		}
+
+
+	}
+
+
+	return Max;
+
+
+}
+
+void Sort(int mas[], int n)
+{
+	int boof = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++) {
+			if (mas[j] > mas[j + 1])
+			{
+				boof = mas[j];
+				mas[j] = mas[j + 1];
+				mas[j + 1] = boof;
+			}
+		}
+	}
+}
+
+void UniqueRand(int mas[], int n, int minRand, int maxRand)
+{
+	//maxRand++;
+	FillRand(mas, n, minRand, maxRand);
+	//srand(time(NULL));
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (mas[j] == mas[i] && i != j)
+			{
+				mas[i] = minRand + rand() % (maxRand - minRand);
+				j = 0;
+			}
+		}
+	}
+}
+
+void Search(int mas[], int n)
+{
+	cout << endl;
+	int coll = 0, ogr = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (mas[i] == mas[j])
+			{
+				coll++;
+			}
+
+
+		}
+		if (coll > 0 && mas[i] != ogr)
+		{
+			ogr = mas[i];
+			cout << mas[i] << "|" << coll;
+		}
+		coll = 0;
+		cout << "\t";
+	}
+
+}
+
+int menu()
+{
+	char menu[6][50] = { "Вывод массива и работа с ним","Движение массива","Сортировка массива","Заполнение массива уник числами","Нахожденеи повторений","Выход" };//Пункты меню
+	int poz = 0, key;//поз-пункта меню, кей-кнопка нажатия
+
+	for (int i = 0; i < 6; i++)//Рисовка меню
+	{
+		SetCursor(15, 10 + i);
+		cout << menu[i];
+
+	}
+	do {
+		for (int i = 0; i < 6; i++)//изменения цвета выбронного пункта
+		{
+			SetCursor(15, 10 + i);
+
+			if (i == poz)
+			{
+				SetColor(Yellow, Black);
+			}
+			else
+			{
+				SetColor(White, Black);
+			}
+
+			cout << menu[i];
+
+		}
+
+		key = _getch();
+		switch (key)
+		{
+		case Up:
+		{	poz--;
+		if (poz < 0)
+		{
+			poz = 5;
+		}
+		break;
+		}
+		case Down:
+		{
+			poz++;
+			if (poz > 5)
+			{
+				poz = 0;
+			}
+			break;
+		}
+		case Enter:
+		{
+			SetColor(White, Black);
+			return(poz);//возвращение выбронного пункта меню
+		}
+
+
+		}
+
+	} while (true);
+
 
 
 
--- Functions/Functions/main.cpp
+++ Functions/Functions/main.cpp
@@ -1,24 +1,36 @@
 ﻿#include<iostream>
 using namespace std;
 
-int Add(int a, int b);	//Прототип функции (Объявление функции - Function declaration)
+int Add(int a, int b = 0, int c = 0);	//Прототип функции (Объявление функции - Function declaration)
 int Sub(int a, int b);
 int Mul(int a, int b);
 int Div(int a, int b);
+void ASCII()
+{
+	cout << "Самурай без меча подобен самураю с мечем ;-)" << endl;
+}
 
 void main()
 {
 	setlocale(LC_ALL, "");
+	ASCII();
 	int a, b;
 	cout << "Введите два числа: "; cin >> a >> b;
-	int c = Add(a, b);	//Использование функции (Вызов функции - Function call)
+	int c = Add(a, b, 5);	//Использование функции (Вызов функции - Function call)
 	cout << a << " + " << b << " = " << c << endl;
 	cout << a << " - " << b << " = " << Sub(a, b) << endl;
 	cout << a << " * " << b << " = " << Mul(a, b) << endl;
 	cout << a << " / " << b << " = " << Div(a, b) << endl;
+
+	//Too few arguments in function call
+	//Too many arguments in function call
+	//Function doesn't take N arguments
+
+	//				Default parameters
+	//Redefinition of default parameter
 }
 
-int Add(int a, int b)	//Реализация функции (Определение функции - Function definition)
+int Add(int a, int b, int c)	//Реализация функции (Определение функции - Function definition)
 {
 	//Addition - сложение
 	int sum = a + b;
