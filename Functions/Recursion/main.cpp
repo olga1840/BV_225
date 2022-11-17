ea375fb3efb05e95285c3b19af07ef4820464214
Clayman <Clayman@DESKTOP-DM8E77U>
2022-11-17 17:32:14 +0200

    Recursion day 1

---
+++ Functions/Recursion/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void elevator(int floor);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Hello Recursion" << endl;
+	int n;
+	cout << "Введите номер этажа: "; cin >> n;
+	elevator(n);
+}
+
+void elevator(int floor)
+{
+	if (floor == 0)
+	{
+		cout << "Вы в подвале" << endl;
+		return;
+	}
+	cout << "Вы на " << floor << " этаже" << endl;
+	elevator(floor - 1);
+	cout << "Вы на " << floor << " этаже" << endl;
+}
+
+/*
+	Stack (Стопка) - это область памяти, из которой последний записанный элемент считывается первым.
+	Stack overflow exception
+*/
